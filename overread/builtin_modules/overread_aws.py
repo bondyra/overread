4d7524dec200bb41836010ee937120bba72eb162
Jakub Bondyra (10C00376) <jb10193@gmail.com>
2024-03-26 01:25:57 +0100

    fix: Speed up AWS module by moving away from cloudcontrol

--- overread/builtin_modules/overread_aws.py
+++ overread/builtin_modules/overread_aws.py
@@ -35,40 +35,12 @@ def _session(place):
 
 # interface member
 async def get(thing_type, place):
-    async with _session(place).client("cloudcontrol") as c:
-        thing = _config["things"][thing_type]
-        resource_model = thing.get("resource_model")
-        if resource_model:
-            results = await _get(c, thing["cc_type_name"])
-            for i in range(len(results)):
-                new_item = results[i][1]
-                for attr, v in resource_model.items():
-                    new_item[attr] = [
-                        r
-                        for _, r in await _get(
-                            c, v["cc_type_name"], ResourceModel=f'{{"{v["parent_field"]}":"{results[i][0]}"}}'
-                        )
-                    ]
-                yield results[i][0], new_item
-        else:
-            for id, it in await _get(c, thing["cc_type_name"]):
-                yield id, it
-
-
-async def _get(c, cloudcontrol_type_name, **list_resources_kwargs):
-    ids = await _list_ids(c, cloudcontrol_type_name, **list_resources_kwargs)
-    return [item async for item in _get_resources(c, ids, cloudcontrol_type_name)]
-
-
-async def _list_ids(client, cloudcontrol_type_name, **kwargs):
-    response = await client.list_resources(TypeName=cloudcontrol_type_name, **kwargs)
-    return [rd["Identifier"] for rd in response.get("ResourceDescriptions", [])]
-
-
-async def _get_resources(client, ids, cloudcontrol_type_name):
-    for i in ids:
-        item = await client.get_resource(TypeName=cloudcontrol_type_name, Identifier=i)
-        yield i, json.loads(item["ResourceDescription"]["Properties"])
+    thing = _config["things"][thing_type]
+    
+    async with _session(place).client(thing["client"]) as c:
+        response = await getattr(c, thing["method"])()
+        for item in response[thing["field"]]:
+            yield item[thing["id"]], item
 
 
 # interface member
@@ -78,14 +50,7 @@ def thing_types():
 
 # interface member
 def prettify(thing_type, thing):
-    result = {da: thing[da] for da in _config["things"][thing_type].get("default_attrs", []) if da in thing}
-    resource_model = _config["things"][thing_type].get("resource_model")
-    if resource_model:
-        for rm_attr, v in resource_model.items():
-            result[rm_attr] = [
-                {da: thing_it[da] for da in v["default_attrs"] if da in thing_it} for thing_it in thing.get(rm_attr, [])
-            ]
-    return result
+    return {da: thing[da] for da in _config["things"][thing_type].get("defaults", []) if da in thing}
 
 
 # interface member
