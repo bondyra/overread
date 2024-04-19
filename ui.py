e21dded9d4e64a14ba2ae3da3d3dd7fc251f8fbd
Jakub Bondyra <jb10193@gmail.com>
2024-04-20 00:11:10 +0200

    wip

--- ui.py
+++ ui.py
@@ -1,172 +1,128 @@
-from nicegui import ui
+from nicegui import ui, context
+from nicegui.events import KeyEventArguments
 
-# from rich.markup import escape
-# from rich.text import Text
-
-# from textual.app import App, ComposeResult
-# from textual.binding import Binding
-# from textual.containers import Container, ScrollableContainer
-# from textual.reactive import reactive
-# from textual.suggester import SuggestFromList
-# from textual.widgets import (Button, Footer, Input, Static, Tree)
+from dataclasses import dataclass
 
 from overread.service import load_modules
 
+from typing import Dict
 
-# from_markup = Text.from_markup
+x, y, c = None, None, None
 
-
-# class Body(ScrollableContainer):
-#     pass
-
-
-# class Message(Static):
-#     pass
+@dataclass
+class Resource:
+    id: str
+    data: Dict
 
 
-# class Section(Container):
-#     pass
+class ccard(ui.card):
+    def __init__(self, res: Resource, posX, posY) -> None:
+        super().__init__()
+        self.item = res
+        with self.props('draggable').classes('w-full cursor-pointer bg-grey-1').style(f"position: absolute; top: {posX}px; left: {posY}px;"):
+            ui.input('Name')
+            ui.tree(dict_to_tree(res.data))
+        self.on('dragstart', self.handle_dragstart)
+        self.on('dragover.prevent', self.highlight)
+        self.on('dragleave', self.unhighlight)
+
+    def handle_dragstart(self) -> None:
+        global dragged
+        dragged = self
+
+    def highlight(self) -> None:
+        self.classes(remove='bg-grey-1', add='bg-red-1')
+
+    def unhighlight(self) -> None:
+        self.classes(remove='bg-red-1', add='bg-grey-1')
 
 
-# class ResourceData(Tree):
-#     def __init__(self, resource_name, resource_data, *args, **kwargs):
-#         super().__init__(resource_name, *args, **kwargs)
-#         def add_node(name, node, data) -> None:
-#             if isinstance(data, dict):
-#                 node.set_label(Text(name))
-#                 for key, value in data.items():
-#                     new_node = node.add("")
-#                     add_node(key, new_node, value)
-#             elif isinstance(data, list):
-#                 node.set_label(Text(f"[] {name}"))
-#                 for index, value in enumerate(data):
-#                     new_node = node.add("")
-#                     add_node(str(index), new_node, value)
-#             else:
-#                 node.allow_expand = False
-#                 if name:
-#                     label = Text.assemble(
-#                         Text.from_markup(f"{name}: {data}")
-#                     )
-#                 else:
-#                     label = Text(repr(data))
-#                 node.set_label(label)
-
-#         add_node(resource_name, self.root, resource_data)
+data1 = {"id1": {"f1": "v1", "f2": ["v2", "v3"], "f3": {"f4": {"f5": "v4"}}}}
+data2 = {"id2": {"f1": "v1", "f2": ["v2", "v3"], "f3": {"f4": {"f5": "v4"}}}}
 
 
-# class ElementContainer(Container):
-#     pass
+def dict_to_tree(inp):
+    if isinstance(inp, dict):
+        return [{'id': k, 'children': dict_to_tree(v)} for k, v in inp.items()]
+    elif isinstance(inp, list):
+        return [{'id': i, 'children': dict_to_tree(v)} for i, v in enumerate(inp)]
+    else:
+        return {'id': str(inp)}
 
 
-# class Vertex(Container):
-#     def __init__(self, resource_name, resource_data, where, *args, **kwargs):
-#         self._resource_name = resource_name
-#         self._resource_data = resource_data
-#         self._where = where
-#         self._suggestions = list(self._data_to_suggestions(self._resource_data))
-#         super().__init__(*args, **kwargs)
+class cunt(ui.row):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-#     def compose(self) -> ComposeResult:
-#         yield ElementContainer(
-#             Static(self._resource_name, classes="label"),
-#             Input(None, "search", suggester=SuggestFromList(self._suggestions)),
-#             ResourceData("data", self._resource_data),
-#             Input(None, "link", suggester=SuggestFromList(self._suggestions)),
-#         )
+    def highlight(self):
+        self.classes(remove='bg-zinc-600', add='bg-yellow-500')
 
-#     def _data_to_suggestions(self, data):
-#         if isinstance(data, dict):
-#             for key in data.keys():
-#                 yield key
-#                 yield from self._data_to_suggestions(data[key])
-#         elif isinstance(data, list):
-#             for item in data:
-#                 yield from self._data_to_suggestions(item)
-#         else:
-#             yield data
+    def unhighlight(self):
+        self.classes(remove='bg-yellow-500', add='bg-zinc-600')
+
+    def load(self):
+        with self:
+            ui.input('Name')
+            ui.tree(dict_to_tree(data1))
 
 
-# class Overread(App[None]):
-#     CSS_PATH = "ui.tcss"
-#     TITLE = "Overread"
-#     BINDINGS = [
-#         ("ctrl+s", "app.screenshot()", "Screenshot"),
-#         Binding("up", "navigate(-1,0)", "Move Up", False),
-#         Binding("down", "navigate(1,0)", "Move Down", False),
-#         Binding("left", "navigate(0,-1)", "Move Left", False),
-#         Binding("right", "navigate(0,1)", "Move Right", False),
-#         Binding("ctrl+c", "app.quit", "Quit", show=True),
-#     ]
+class dupagrid(ui.grid):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(columns=32, rows=32, *args, **kwargs)
+        with self.classes('w-full gap-5'):
+            self._cunts = [
+                [cunt().classes('col-span-1 borderless rounded p-1 bg-zinc-600') for _ in range(32)]
+                for _ in range(32)
+            ]
 
-#     show_sidebar = reactive(False)
+    def at(self, i, j):
+        return self._cunts[i][j] if i < 32 and j < 32 and i >= 0 and j >= 0 else None
 
-#     def __init__(self, modules, *args, **kwargs):
-#         self._modules = modules
-#         super().__init__(*args, **kwargs)
 
-#     def compose(self) -> ComposeResult:
-#         yield Container(
-#             Body(
-#                 Section(
-#                     Vertex("cos", {"dupaMark": {"mleczko": {"chujKurwa": "mleko123"}}}, "default/us-east-1"),
-#                 ),
-#                 classes="location-widgets location-first",
-#             ),
-#         )
-#         yield Footer()
-
-#     def on_mount(self) -> None:
-#         self.query_one("Vertex", Vertex).focus()
-
-#     def action_screenshot(self, filename = None, path = "./"):
-#         path = self.save_screenshot(filename, path)
-#         message = f"Screenshot saved to [bold green]'{escape(str(path))}'[/]"
-#         self.notify(message)
-
+context.get_client().content.classes('h-[100vh]')  # 1
+ui.add_head_html('<style>.q-textarea.flex-grow .q-field__control { height: 100% }</style>')  # 2
 
 modules = load_modules()
-ui.label("Kurwa!")
-ui.label("Cipa!")
-with ui.row().style('display: block;'):
-    with ui.column().style('display: block;'):
-        with ui.card():
-            ui.input('Name')
-            ui.tree(
-                [
-                    {'id': 'numbers', 'children': [{'id': '1'}, {'id': '2'}]},
-                    {'id': 'letters', 'children': [{'id': 'A'}, {'id': 'B'}]},
-                ], 
-                label_key='id'
-            )
-        with ui.card():
-            ui.input('Name')
-            ui.tree(
-                [
-                    {'id': 'numbers', 'children': [{'id': '1'}, {'id': '2'}]},
-                    {'id': 'letters', 'children': [{'id': 'A'}, {'id': 'B'}]},
-                ], 
-                label_key='id'
-            )
-    with ui.column().style('display: block;'):
-        ui.html('<svg width="32" height="500"><line x1="10" y1="10" x2="190" y2="10" style="stroke: gray; stroke-width: 5;" /></svg>')
-    with ui.column().style('display: block;'):
-        with ui.card():
-            ui.input('Name')
-            ui.tree(
-                [
-                    {'id': 'numbers', 'children': [{'id': '1'}, {'id': '2'}]},
-                    {'id': 'letters', 'children': [{'id': 'A'}, {'id': 'B'}]},
-                ], 
-                label_key='id'
-            )
-        with ui.card():
-            ui.input('Name')
-            ui.tree(
-                [
-                    {'id': 'numbers', 'children': [{'id': '1'}, {'id': '2'}]},
-                    {'id': 'letters', 'children': [{'id': 'A'}, {'id': 'B'}]},
-                ], 
-                label_key='id'
-            )
+ui.label("overread 0.2")
+ui.separator().classes("w-full")
+
+
+def handle_key(e: KeyEventArguments):
+    if e.action.keydown:
+        if e.key.arrow_left:
+            select(0, -1)
+        elif e.key.arrow_right:
+            select(0, 1)
+        elif e.key.arrow_up:
+            select(-1, 0)
+        elif e.key.arrow_down:
+            select(1, 0)
+        elif e.key.enter:
+            load()
+
+
+keyboard = ui.keyboard(on_key=handle_key)
+
+with ui.scroll_area().classes('w-full h-full no-wrap'):
+    d = dupagrid().classes("flex-grow")
+
+
+def select(i, j):
+    global x, y, c
+    newx, newy = (x or 0) + i, (y or 0) + j
+    cc = d.at(newx, newy)
+    if cc:
+        cc.highlight()
+        if c:
+            c.unhighlight()
+        x, y, c = newx, newy, cc
+
+
+def load():
+    global c
+    if c:
+        c.load()
+
+
+select(0, 0)
 ui.run(dark=True)
